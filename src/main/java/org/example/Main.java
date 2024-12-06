438c9cce495f865c6c152d968039a5b7e104fc88
gleb <gleboshkay@list.ru>
2024-12-06 12:04:11 +0500

    Решил 5 задачу

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,19 @@
 package org.example;
 
 
-import org.example.task.Task1;
-import org.example.task.Task2;
-import org.example.task.Task4;
-import org.example.task.Task7;
+import org.example.task.*;
 
 public class Main {
     public static void main(String[] args) {
-        Task1.compare(3310, 1033);
-        Task2.sort("1+1+3+1+3");
-        Task4.findIndexByNumber(7);
-        Task4.findIndexByNumberSimple(7);
-        Task7.seat(1);
-        Task7.seat(2);
-        Task7.seat(3);
-        Task7.seat(6);
+//        Task1.compare(3310, 1033);
+//        Task2.sort("1+1+3+1+3");
+//        Task4.findIndexByNumber(7);
+//        Task4.findIndexByNumberSimple(7);
+//        Task7.seat(1);
+//        Task7.seat(2);
+//        Task7.seat(3);
+//        Task7.seat(6);
+
+        Task5.find();
     }
 }
---
+++ src/main/java/org/example/task/Task5.java
@@ -0,0 +1,71 @@
+package org.example.task;
+
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task5 {
+    public static void find() {
+        List<String> availableAdresses = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File("src/main/resources/input.txt"))) {
+            while (scanner.hasNextLine()) {
+                String address = scanner.nextLine();
+                if (isAvailable(address)) {
+                    availableAdresses.add(address);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        writeOutput(availableAdresses);
+
+        openAddress(availableAdresses.get(0));
+    }
+
+    private static boolean isAvailable(String name) {
+        try {
+            URL url = new URL("https://" + name);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(500);
+            return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    private static void writeOutput(List<String> addresses) {
+        try {
+            Files.createFile(Path.of("src/main/resources/output.txt"));
+            File output = new File("src/main/resources/output.txt");
+            try (FileWriter outputWriter = new FileWriter(output, true)) {
+                addresses.forEach(address -> {
+                    try {
+                        outputWriter.write(address + "\n");
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Something went wrong");
+        }
+    }
+
+    private static void openAddress(String address) {
+        try {
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                Desktop.getDesktop().browse(new URI("https://" + address));
+            }
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
